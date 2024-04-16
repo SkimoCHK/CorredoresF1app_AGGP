9d015912d40cfc99047c3ce90183a730f2e62bb6
eleduard0 <arturo.13040570@gmail.com>
2024-04-15 23:47:00 -0700

    arduino

--- CorredoresF1app_AGGP/CorredoresF1app_AGGP/View/EditarArea.xaml.cs
+++ CorredoresF1app_AGGP/CorredoresF1app_AGGP/View/EditarArea.xaml.cs
@@ -7,16 +7,80 @@ using CorredoresF1app_AGGP.ViewModel;
 using CorredoresF1app_AGGP.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
 
 namespace CorredoresF1app_AGGP.View
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class EditarArea : ContentPage
 	{
+		string server;
+		Int32 port;
+		TcpClient tcpClient;
+		Byte[] bytes;
+		NetworkStream stream;
+		string entrada = "";
+		bool conectando= false;
+		bool conectado = false;
+		
+
 		public EditarArea(Area area)
 		{
 			InitializeComponent ();
 			BindingContext = new VMeditarArea(Navigation, area);
+
+			server = "192.168.137.112";
+			port = 8888;
+
+			sw0.IsEnabled = false;
+
+			Device.StartTimer(TimeSpan.FromMilliseconds(500), () =>
+			{
+				if (conectando == false)
+				{
+					_ = IniciarConecte();
+				}
+				if (conectado == true)
+				{
+					sw0.IsEnabled = true;
+				}
+
+				return true;
+			});
+
+
+
+        }
+
+		public async Task IniciarConecte()
+		{
+			lblStatus.Text = "Conectando...";
+			tcpClient = new TcpClient();
+			conectando = true;
+			await tcpClient.ConnectAsync(server,port);
+			stream = tcpClient.GetStream();
+			lblStatus.Text = "Conectado";
 		}
-	}
+		int a = 0;
+		string message;
+        private void sw0_Toggled(object sender, ToggledEventArgs e)
+        {
+			if (sw0.IsToggled)
+			{
+				a = 1;
+			}
+			else
+			{
+				a = 0;
+			}
+			message = "0" + a + Environment.NewLine;
+			Console.WriteLine("Valvula:" + message);
+
+			bytes = System.Text.Encoding.ASCII.GetBytes(message);
+			stream.Write(bytes, 0, bytes.Length);
+        }
+    }
 }
